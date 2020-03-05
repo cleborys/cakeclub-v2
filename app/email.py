e1957f71f58d59d900c60f38c83f7e84f4f6a1bc
Clemens Borys <clemens@borys.dk>
2020-03-05 16:06:28 +0100

    log scheduling and sending of emails

--- app/email.py
+++ app/email.py
@@ -8,6 +8,7 @@ from threading import Thread
 def send_email_async(app, message):
     with app.app_context():
         mail.send(message)
+    app.logger.info(f"Sent email to {message.recipients} about '{message.subject}'.")
 
 
 def send_email(subject, sender, recipients, body, plain=None):
@@ -17,6 +18,7 @@ def send_email(subject, sender, recipients, body, plain=None):
         msg.body = body
     else:
         msg.body = plain
+    current_app.logger.info(f"Scheduling email to {recipients} about '{subject}'.")
     Thread(
         target=send_email_async, args=(current_app._get_current_object(), msg)
     ).start()
